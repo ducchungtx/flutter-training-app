0fa139a16597d962c648dd6a8e9072f125444091
Chung Nguyen <zrmedia9@gmail.com>
2021-12-10 21:22:42 +0700

    Update Home page UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -63,7 +63,87 @@ class _HomePageState extends State<HomePage> {
                 Icon(Icons.arrow_forward,
                     size: 20, color: color.AppColor.homePageIcons),
               ],
-            )
+            ),
+            SizedBox(height: 20),
+            Container(
+              width: MediaQuery.of(context).size.width,
+              height: 220,
+              decoration: BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    color.AppColor.gradientFirst.withOpacity(0.8),
+                    color.AppColor.gradientSecond.withOpacity(0.9),
+                  ], begin: Alignment.bottomLeft, end: Alignment.centerRight),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(10),
+                    bottomLeft: Radius.circular(10),
+                    bottomRight: Radius.circular(10),
+                    topRight: Radius.circular(80),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                        color: color.AppColor.gradientSecond.withOpacity(0.2),
+                        blurRadius: 20,
+                        offset: Offset(5, 10))
+                  ]),
+              child: Container(
+                padding: const EdgeInsets.only(top: 25, left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Next workout",
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: color.AppColor.homePageContainerTextSmall)),
+                    SizedBox(height: 5),
+                    Text("Legs Toning",
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: color.AppColor.homePageContainerTextSmall)),
+                    SizedBox(height: 5),
+                    Text("and Glutes Workout",
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: color.AppColor.homePageContainerTextSmall)),
+                    SizedBox(height: 25),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            Icon(Icons.timer,
+                                size: 20,
+                                color:
+                                    color.AppColor.homePageContainerTextSmall),
+                            SizedBox(width: 5),
+                            Text("60 min",
+                                style: TextStyle(
+                                    fontSize: 14,
+                                    color: color
+                                        .AppColor.homePageContainerTextSmall)),
+                          ],
+                        ),
+                        Expanded(child: Container()),
+                        Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(60),
+                              boxShadow: [
+                                BoxShadow(
+                                    color: color.AppColor.gradientFirst
+                                        .withOpacity(0.8),
+                                    blurRadius: 10,
+                                    offset: Offset(4, 8))
+                              ]),
+                          child: Icon(Icons.play_circle_fill,
+                              size: 60,
+                              color: color.AppColor.homePageContainerTextSmall),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
           ],
         ),
       ),
