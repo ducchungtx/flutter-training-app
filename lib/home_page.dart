90c091b4c182ebb2a398e126d27812e3b9c8c1d8
Chung Nguyen <zrmedia9@gmail.com>
2021-12-11 10:09:52 +0700

    Done Home page UI

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'colors.dart' as color;
 
@@ -9,6 +11,20 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List info = [];
+
+  _initData() {
+    DefaultAssetBundle.of(context).loadString("json/info.json").then((value) {
+      info = json.decode(value);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -143,7 +159,178 @@ class _HomePageState extends State<HomePage> {
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            SizedBox(height: 5),
+            Container(
+              height: 180,
+              width: MediaQuery.of(context).size.width,
+              child: Stack(
+                children: <Widget>[
+                  Container(
+                    margin: const EdgeInsets.only(top: 30),
+                    width: MediaQuery.of(context).size.width,
+                    height: 120,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                          image: AssetImage("assets/card.jpg"),
+                          fit: BoxFit.fill,
+                        ),
+                        boxShadow: [
+                          BoxShadow(
+                              color:
+                                  color.AppColor.gradientFirst.withOpacity(0.3),
+                              blurRadius: 40,
+                              offset: Offset(8, 10)),
+                          BoxShadow(
+                              color:
+                                  color.AppColor.gradientFirst.withOpacity(0.3),
+                              blurRadius: 10,
+                              offset: Offset(-1, -5))
+                        ]),
+                  ),
+                  Container(
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                    margin: const EdgeInsets.only(right: 200, bottom: 30),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        image: DecorationImage(
+                            image: AssetImage('assets/figure.png'))),
+                  ),
+                  Container(
+                    width: double.maxFinite,
+                    height: 100,
+                    margin: const EdgeInsets.only(left: 150, top: 50),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          "You are doing great",
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: color.AppColor.homePageDetail),
+                        ),
+                        SizedBox(height: 10),
+                        RichText(
+                            text: TextSpan(
+                                text: "Keep it up\n",
+                                style: TextStyle(
+                                    color: color.AppColor.homePagePlanColor,
+                                    fontSize: 16),
+                                children: [
+                              TextSpan(
+                                text: "stick to your plan",
+                                style: TextStyle(
+                                    color: color.AppColor.homePagePlanColor,
+                                    fontSize: 16),
+                              )
+                            ]))
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              children: <Widget>[
+                Text(
+                  "Area of focus",
+                  style: TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.w500,
+                      color: color.AppColor.homePageTitle),
+                )
+              ],
+            ),
+            Expanded(
+                child: OverflowBox(
+              maxWidth: MediaQuery.of(context).size.width,
+              child: MediaQuery.removePadding(
+                removeTop: true,
+                context: context,
+                child: ListView.builder(
+                    itemCount: info.length.toDouble() ~/ 2,
+                    itemBuilder: (_, i) {
+                      int a = 2 * i; // 0, 2
+                      int b = 2 * i + 1; // 1, 3
+                      return Row(
+                        children: <Widget>[
+                          Container(
+                            width: (MediaQuery.of(context).size.width - 90) / 2,
+                            height: 170,
+                            margin: const EdgeInsets.only(
+                                left: 30, bottom: 15, top: 15),
+                            padding: const EdgeInsets.only(bottom: 5),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                    image: AssetImage(info[a]['img'])),
+                                boxShadow: [
+                                  BoxShadow(
+                                      blurRadius: 3,
+                                      offset: Offset(5, 5),
+                                      color: color.AppColor.gradientSecond
+                                          .withOpacity(0.1)),
+                                  BoxShadow(
+                                      blurRadius: 3,
+                                      offset: Offset(-5, -5),
+                                      color: color.AppColor.gradientSecond
+                                          .withOpacity(0.1))
+                                ]),
+                            child: Center(
+                              child: Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Text(
+                                  info[a]['title'],
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: color.AppColor.homePageDetail),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            width: (MediaQuery.of(context).size.width - 90) / 2,
+                            height: 170,
+                            padding: const EdgeInsets.only(bottom: 5),
+                            margin: const EdgeInsets.only(
+                                left: 30, bottom: 15, top: 15),
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: BorderRadius.circular(15),
+                                image: DecorationImage(
+                                    image: AssetImage(info[b]['img'])),
+                                boxShadow: [
+                                  BoxShadow(
+                                      blurRadius: 3,
+                                      offset: Offset(5, 5),
+                                      color: color.AppColor.gradientSecond
+                                          .withOpacity(0.1)),
+                                  BoxShadow(
+                                      blurRadius: 3,
+                                      offset: Offset(-5, -5),
+                                      color: color.AppColor.gradientSecond
+                                          .withOpacity(0.1))
+                                ]),
+                            child: Center(
+                              child: Align(
+                                alignment: Alignment.bottomCenter,
+                                child: Text(
+                                  info[b]['title'],
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: color.AppColor.homePageDetail),
+                                ),
+                              ),
+                            ),
+                          )
+                        ],
+                      );
+                    }),
+              ),
+            ))
           ],
         ),
       ),
---
+++ lib/video_info.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class VideoInfo extends StatefulWidget {
+  const VideoInfo({Key? key}) : super(key: key);
+
+  @override
+  _VideoInfoState createState() => _VideoInfoState();
+}
+
+class _VideoInfoState extends State<VideoInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
